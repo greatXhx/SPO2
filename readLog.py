f04d2415499fc74bd34236d7e42d4c276ce7ce67
Xu <xuhaixiang103@163.com>
2018-07-26 22:35:48 +0800

    读取log文件中的红外和红光数据

---
+++ readLog.py
@@ -0,0 +1,18 @@
+def readLog(path):
+    import re
+
+    line = []
+    f = open(path)  # 返回一个文件对象
+    lines = f.readlines()#读取全部内容
+    reg = r'SPO2DeviceStream2: (.{12})'#只取SessionId=字符后面32位字符串
+    wordreg = re.compile(reg)
+    wordreglist = []
+    aun_ir_buffer = []
+    aun_red_buffer = []
+    for line in lines:
+        if re.findall(wordreg, line):
+            wordreglist.append(re.findall(wordreg, line))
+            aun_ir_buffer.append(int(re.findall(wordreg, line)[0][:5]))
+            aun_red_buffer.append(int(re.findall(wordreg, line)[0][6:]))
+
+    return aun_ir_buffer, aun_red_buffer
