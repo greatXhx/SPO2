2e6713e53dcf41bdd1f23ee061918fec75051553
Xu <xuhaixiang103@163.com>
2018-07-27 11:21:54 +0800

    读取Android端log文件中的红光和红外光数据

--- readLog.py
+++ readLog.py
@@ -12,7 +12,7 @@ def readLog(path):
     for line in lines:
         if re.findall(wordreg, line):
             wordreglist.append(re.findall(wordreg, line))
-            aun_ir_buffer.append(int(re.findall(wordreg, line)[0][:5]))
-            aun_red_buffer.append(int(re.findall(wordreg, line)[0][6:]))
+            aun_red_buffer.append(int(re.findall(wordreg, line)[0][:5]))
+            aun_ir_buffer.append(int(re.findall(wordreg, line)[0][6:]))
 
-    return aun_ir_buffer, aun_red_buffer
+    return aun_red_buffer, aun_ir_buffer
